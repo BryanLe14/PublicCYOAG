060b1a5d8800c1cc6b6e7884257fee3e1a5f0a1c
ElNayrb <99197660+BryanLe14@users.noreply.github.com>
2022-09-12 19:32:30 +0000

    9/12/2022 @ 1400

--- ascii_map.py
+++ ascii_map.py
@@ -169,7 +169,9 @@ https://www.w3.org/TR/xml-entity-names/025.html
 """
 
 import math
+import time
 from universal_globals import *
+from getkey import getkey, keys
 
 ascii_map = []
 
@@ -181,20 +183,12 @@ for y in range(height-1):
 
 # print("".join(ascii_map))
 
+block_size = 2
 
 ascii_graphics_data = {
-    '&': [
-        "&&",
-        "&&"
-    ],
-    '#': [
-        "##",
-        "##"
-    ],
-    '.': [
-        "..",
-        ".."
-    ],
+    '&': ['&' * block_size for i in range(block_size)],
+    '#': ['#' * block_size for i in range(block_size)],
+    '.': ['.' * block_size for i in range(block_size)],
 }
 
 map_data = {
@@ -233,29 +227,38 @@ def createRoom():
 
 formatted_map_list = []
 for y_count, y_value in enumerate(map_data["map"]):
-    yc2 = (y_count * 2)
-    # formatted_map_list[yc2] = formatted_map_list[(yc2 + 1)] = ""
-    formatted_map_list.append("")
-    formatted_map_list.append("")
+    yc2 = (y_count * block_size)
+    for yi in range(block_size):
+        formatted_map_list.append("")
     for x_count, x_value in enumerate(y_value):
-        formatted_map_list[yc2] += ascii_graphics_data[x_value][0]
-        formatted_map_list[(yc2 + 1)] += ascii_graphics_data[x_value][1]
+        for xi in range(block_size):
+            formatted_map_list[(yc2 + xi)] += ascii_graphics_data[x_value][xi]
 
 # print(formatted_map_list)
 
-x = 0
+x = 10
 y = 0
 
-formatted_map_str = ""
+def main():
+    
+    formatted_map_str = ""
+    
+    for i in range(y - math.floor(height2), -1):
+        formatted_map_str += "▓" * width + "\n"
+    for y_count, y_value in enumerate(formatted_map_list):
+        for i in range(x - math.floor(width2), -1):
+            formatted_map_str += "▓"
+        for x_count, x_value in enumerate(y_value):
+            if x_count > x - width2 and x_count < x + width2 and y_count > y - height2 and y_count < y + height2:
+                formatted_map_str += x_value
+        formatted_map_str += "\n"
+    
+    print(formatted_map_str)
 
-for i in range(y - math.floor(height2), -1):
-    formatted_map_str += "▓" * width + "\n"
-for y_count, y_value in enumerate(formatted_map_list):
-    for i in range(x - math.floor(width2), -1):
-        formatted_map_str += "▓"
-    for x_count, x_value in enumerate(y_value):
-        if x_count > x - width2 and x_count < x + width2 and y_count > y - height2 and y_count < y + height2:
-            formatted_map_str += x_value
-    formatted_map_str += "\n"
 
-print(formatted_map_str)
+while True:
+    key = getKey()
+    if key == keys.RIGHT:
+        x += 1
+    main()
+    time.sleep(0.5)
