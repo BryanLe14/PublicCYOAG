05229865ff269773416a043834e8844d405f16a7
ElNayrb <99197660+BryanLe14@users.noreply.github.com>
2022-09-14 19:23:15 +0000

    Mid-dev version

--- ascii_map.py
+++ ascii_map.py
@@ -185,13 +185,12 @@ for y in range(height-1):
 
 # print("".join(ascii_map))
 
-block_size = 2
 player_char = "@"
 
 ascii_graphics_data = {
-    '&': ['&&' * block_size for i in range(block_size)],
-    '#': ['##' * block_size for i in range(block_size)],
-    '.': ['.' * block_size for i in range(block_size)],
+    '&': '&',
+    '#': '#',
+    '.': '.',
 }
 
 map_data = {
@@ -209,12 +208,10 @@ def createRoom():
 
 formatted_map_list = []
 for y_count, y_value in enumerate(map_data["map"]):
-    yc2 = (y_count * block_size)
-    for yi in range(block_size):
-        formatted_map_list.append("")
+    yc2 = (y_count)
+    formatted_map_list.append("")
     for x_count, x_value in enumerate(y_value):
-        for xi in range(block_size):
-            formatted_map_list[(yc2 + xi)] += ascii_graphics_data[x_value][xi]
+        formatted_map_list[yc2] += ascii_graphics_data[x_value]
 
 # print(formatted_map_list)
 
@@ -227,25 +224,37 @@ def ascii_map_main():
     clear_screen()
         
     formatted_map_str = ""
+    # Add whitespace to the top
     for i in range(y - math.floor(height2), -1):
-        formatted_map_str += "▓" * width + "\n"
+        formatted_map_str += (("▓" * width + "\n") if (i == -2) else ("\n"))
     for y_count, y_value in enumerate(formatted_map_list):
+        # Whitespace left
         for i in range(x - math.floor(width2), -1):
             if y_count >= y - height2 and y_count < y + height2 - 4:
-                formatted_map_str += "▓"
+                formatted_map_str += (("▓") if (i == -2 or i == -3) else ("-"))
+        # Actual stuff
         for x_count, x_value in enumerate(y_value):
             if x_count*2 > x - width2 and x_count*2 < x + width2 and y_count > y - height2 and y_count < y + height2 - 4:
                 char_to_add = x_value if len(x_value) == 2 else x_value + " "
-                formatted_map_str += (player_char+" ") if (x_count == x and y_count == y) else (char_to_add)
+                formatted_map_str += (player_char+" ") if (x_count*2 == x and y_count == y) else (char_to_add)
+        # Whitespace right
+        for i in range(map_data["width"]*2, x + math.floor(width2)+1):
+            if y_count >= y - height2 and y_count < y + height2 - 4 and x > map_data["width"] / 2:
+                formatted_map_str += (("▓") if (i == map_data["width"]+1) else ("-"))
         formatted_map_str += "\n"
+    # Whitespace bottom
+    for i in range(map_data["height"]+1, y + math.floor(height2) - 4):
+        formatted_map_str += "▓" * width + "\n"
+    print(formatted_map_str)
     
-    print(formatted_map_str.strip())
+    with open('levels/currMap.txt', "w") as f:
+        f.write(formatted_map_str)
     
     key = get_key()
     if key == 'left':
-        x -= 1
+        x -= 2
     elif key == 'right':
-        x += 1
+        x += 2
     elif key == 'up':
         y -= 1
     elif key == 'down':
