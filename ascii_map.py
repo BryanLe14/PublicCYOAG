405cc97f35a5c03470982004efff6786eab3d47f
ElNayrb <99197660+BryanLe14@users.noreply.github.com>
2022-09-19 18:34:21 +0000

    Implemented function `check_walls`. Unstable if player goes too far right. Says IndexError, have to debug.

--- ascii_map.py
+++ ascii_map.py
@@ -221,7 +221,9 @@ y = 0
 
 def char_is_wall(char):
     return (char == "#" or char == "▓")
-
+def is_wall(x_pos, y_pos):
+    is_in_bounds = x_pos >= 0 and x_pos < map_data["width"] and y_pos >= 0 and y_pos < map_data["height"]
+    return char_is_wall(formatted_map_list[y_pos][x_pos]) if is_in_bounds else True
 def get_char_on_map(x_pos, y_pos):
     if x == x_pos and y == y_pos:
         return player_char
@@ -229,6 +231,16 @@ def get_char_on_map(x_pos, y_pos):
         is_in_bounds = x_pos >= 0 and x_pos < map_data["width"] and y_pos >= 0 and y_pos < map_data["height"]
         return formatted_map_list[y_pos][x_pos] if is_in_bounds else "▓"
 
+def check_walls(x_count, y_count, up_is_wall, down_is_wall, left_is_wall, right_is_wall):
+    to_left = x_count - 1
+    to_right = x_count + 1
+    to_up = y_count - 1
+    to_down = y_count + 1
+    if (up_is_wall == is_wall(x_count, to_up))\
+     and (down_is_wall == is_wall(x_count, to_down))\
+     and (left_is_wall == is_wall(to_left, y_count))\
+     and (right_is_wall == is_wall(to_right, y_count)):
+        return True
 def ascii_map_main():
     global x, y, formatted_map_str
     
@@ -249,7 +261,56 @@ def ascii_map_main():
                 # if character is two characters long, leave it. else, add a space
                 char_to_add = x_value if len(x_value) == 2 else x_value + " "
                 # if character position is the player's position, replace it
-                formatted_map_str += (player_char+" ") if (x_count*2 == x and y_count == y) else (char_to_add)
+                char_to_add = (player_char+" ") if (x_count*2 == x and y_count == y) else (char_to_add)
+                
+                # walls
+                if char_is_wall(x_value):
+                    """if this is a wall"""
+                    """
+                    elif is_wall(x_count, to_up)\
+                     and is_wall(x_count, to_down)\
+                     and is_wall(to_left, y_count)\
+                     and is_wall(to_right, y_count):
+                        char_to_add  = " "
+                    """
+                    
+                    if check_walls(x_count, y_count, True, True, True, True):
+                        char_to_add  = "╬ "
+                    elif check_walls(x_count, y_count, False, True, True, True):
+                        char_to_add  = "╦ "
+                    elif check_walls(x_count, y_count, True, False, True, True):
+                        char_to_add  = "╩ "
+                    elif check_walls(x_count, y_count, True, True, False, True):
+                        char_to_add  = "╠ "
+                    elif check_walls(x_count, y_count, True, True, True, False):
+                        char_to_add  = "╣ "
+                    elif check_walls(x_count, y_count, False, False, True, True):
+                        char_to_add  = "═ "
+                    elif check_walls(x_count, y_count, True, True, False, False):
+                        char_to_add  = "║ "
+                    """elif char_is_wall(formatted_map_list[y_count-1][x_count]):
+                        ""if connected up""
+                        # if connected left
+                        if char_is_wall(formatted_map_list[y_count][x_count-1]):
+                            char_to_add  = "╝ "
+                        # if connected right
+                        if char_is_wall(formatted_map_list[y_count][x_count+1]):
+                            char_to_add  = "╚ "
+                        # if connected down
+                        if char_is_wall(formatted_map_list[y_count+1][x_count]):
+                            char_to_add  = "║ "
+                    elif char_is_wall(formatted_map_list[y_count-1][x_count]):
+                        ""if connected down""
+                        # if connected left
+                        if char_is_wall(formatted_map_list[y_count][x_count-1]):
+                            char_to_add  = "╗ "
+                        # if connected right
+                        if char_is_wall(formatted_map_list[y_count][x_count+1]):
+                            char_to_add  = "╔ "
+                        # if connected up
+                        if char_is_wall(formatted_map_list[y_count-1][x_count]):
+                            char_to_add  = "║ " """
+                formatted_map_str += char_to_add
         # Whitespace right
         # for i in range(map_data["width"]*2, x + math.floor(width2)+1):
         #     if y_count >= y - height2 and y_count < y + height2 - 4 and x > map_data["width"] / 2:
@@ -269,7 +330,7 @@ def ascii_map_main():
     print("HP: 100\nATK: 10")
     
     key = get_key()
-    if key == 'left' and not char_is_wall(get_char_on_map(math.floor(x/2)-1, y)):
+    if key == 'left' and not is_wall(math.floor(x/2)-1, y):
         x -= 2
         console.log("Player moved left")
     elif key == 'right' and not char_is_wall(get_char_on_map(math.floor(x/2)+1, y)):
